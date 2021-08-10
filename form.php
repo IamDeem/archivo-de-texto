5020c8d751f25bdb2d777e9fa6d71ecaf9091c03
IamDeem <78952501+IamDeem@users.noreply.github.com>
2021-08-09 20:08:11 -0400

    Add files via upload

--- form.php
+++ form.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Archivo de Texto</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/estilomenu.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.min.css">
+    <title> Archivo txt  </title>
 </head>
+        <script src="js/jquery-3.5.1.min.js"></script>
+        <script src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+
 <body>
     <form action="guardar.php" method="post" name="formulario"><br>
 Titulo:<br>
